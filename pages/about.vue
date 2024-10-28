77b146e384273916d77ff4af6ec67f1315648d81
kuomay <guomei88119@gmail.com>
2024-10-28 15:05:44 +0800

    建立page

---
+++ pages/about.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+        <h1>About Us</h1>
+        <p>Welcome to our website! We are a team of passionate individuals dedicated to providing high-quality products and services.</p>
+        <p>Feel free to explore our website and learn more about what we do.</p>
+    </div>
+</template>
+
+<script setup>
+</script>
+
+<style scoped>
+</style>
